54b2af5c72b4e0155d666f18c74e149b649f380f
akashsangal09 <akashsangal09@gmail.com>
2023-07-19 17:12:15 +0530

    testing

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,10 +1,3 @@
 //= require active_admin/base
 //= require activeadmin/quill_editor/quill 
 //= require activeadmin/quill_editor_input
-//= require bootstrap 
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require bootstrap.min
-//= require npm
-//= require_tree .
